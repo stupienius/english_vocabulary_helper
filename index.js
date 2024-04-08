eae00e15ff2f76f88204d06693835fe4d19dd354
stupienius <wangjay0401@gmail.com>
2024-04-08 21:16:04 +0800

    basic js lib display

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 let model = 1;
+
+let lib = ["hello","apple","angry"];
+let word = document.getElementById("word");
+reload();
+
+
+function reload(){
+    for(let i=0;i<lib.length;i++){
+        word.innerHTML += lib[i]+ "<br>" ;
+    }
+}
+
+
