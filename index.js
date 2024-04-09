59e3e528a004a7c7f80c1adc59ef7a32668bcb8e
stupienius <wangjay0401@gmail.com>
2024-04-09 20:42:51 +0800

    onclick is fucking broken

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ reload();
 
 function reload(){
     for(let i=0;i<word.length;i++){
-        lib.innerHTML += "<p>"+word[i]+"</p>" ;
+        lib.innerHTML += `<p onclick = "wordDetail('${word[i]}')">${word[i]}</p>` ;
     }
 }
 
+console.log(lib);
 
+function wordDetail(index) {
+    console.log(index);
+}
+
+// function modelChange(a){
+//     model = a;
+//     console.log(a);
+// }
