ca591cc8cbe2d9f69181c2a0df0781b1eb0c0b5f
stupienius <wangjay0401@gmail.com>
2024-04-14 17:58:44 +0800

    word lidrary

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 let model = 1;
 
-let words = ["hello","apple","banana","tiger"];
+class word {
+
+}
+
+let words = ["hello","apple","banana","tiger",
+            "elephant","ant","height","good","nice",
+            "great","fun","hi","content","finally","last",
+            "apple","banana","tiger",
+            "elephant","ant","height","good","nice",
+            "great","fun","hi","content","finally","last"];
+
+const lib = document.querySelector(".lib");
+
+reload();
+
+function changeMoled(index){
+    console.log(index);
+    model = index ;
+    if(model === 2){
+        reload();
+    }else{
+        lib.style.display = "none";
+    }
+}
+
+function reload(){
+    lib.innerHTML = "";
+    for(let i=0;i<words.length;i++){
+        lib.innerHTML += `<p onclick="detail(${words[i]})">${words[i]}</p>`;
+    }
+    lib.style.display = "block";
+}
+
+
+                                             
 
 function print(a){
-    console.log("hello");
+    console.log("hello"+a);
 }
