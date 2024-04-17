f41cd76e7859e8b1a81d62ec4dd1e651d06592c5
stupienius <wangjay0401@gmail.com>
2024-04-18 05:04:13 +0800

    set up word calss and renew reload function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let model = 1;
 
 class word {
-
+    constructor(name,PoS,ch,eg){
+        this.name = name;
+        this.PoS = PoS;
+        this.ch = ch;
+        this.eg = eg;
+    }
+    
 }
 
 let words = ["hello","apple","banana","tiger",
@@ -12,14 +18,23 @@ let words = ["hello","apple","banana","tiger",
             "great","fun","hi","content","finally","last"];
 
 const lib = document.querySelector(".lib");
+const wel = document.querySelector(".welcome");
+const qui = document.querySelector(".quize");
+const add = document.querySelector(".add");
 
 function changeMoled(index){
-    console.log(index);
+    wel.style.display = "none";
     model = index ;
-    if(model === 2){
-        reload();
-    }else{
+    if(model === 1){
+        qui.style.display = "none";
         lib.style.display = "none";
+    }else if(model === 2){
+        reload();
+        qui.style.display = "none";
+        add.style.display = "none";
+    }else if(model === 3){
+        lib.style.display = "none";
+        add.style.display = "none";
     }
 }
 
